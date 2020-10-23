d2269ae61948c4a97dddb9a2b39bc08b96203569
Steven Harris <38961543+nixintel@users.noreply.github.com>
2020-10-23 12:22:27 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,12 +6,7 @@ import os
 
 load_dotenv()
 
-hibp_key = os.getenv('HIBP_KEY')
-censys_key = os.getenv('CENSYS_KEY')
-censys_secret = os.getenv('CENSYS_SECRET')
-ipinfo_key = os.getenv('IPINFO_SECRET')
 shodan_key = os.getenv('SHODAN_KEY')
-otx_key = os.getenv('OTX_KEY')
 aipdb_key = os.getenv('AIPDB_KEY')
 
 
